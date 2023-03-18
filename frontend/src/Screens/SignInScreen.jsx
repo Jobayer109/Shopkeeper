1fddc93b1f2b55842ea67c7b16d69322bddc1321
user.email <jobayer.ahmed109@gmail.com>
2023-03-18 14:46:27 +0600

    successfully login user by db data

--- frontend/src/Screens/SignInScreen.jsx
+++ frontend/src/Screens/SignInScreen.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import axios from "axios";
+import React, { useContext, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import { Helmet } from "react-helmet-async";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Store } from "../components/Store";
 
 const SignInScreen = () => {
   const { search } = useLocation();
   const redirectInUrl = new URLSearchParams(search).get("redirect");
   const redirect = redirectInUrl ? redirectInUrl : "/";
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("/api/user/signIn", {
+        email,
+        password,
+      });
+      ctxDispatch({ type: "USER_SIGNIN", payload: data });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      navigate(redirect || "/");
+    } catch (error) {
+      alert("Invalid email or password");
+    }
+  };
 
   return (
     <Container className="small-container">
@@ -15,14 +37,24 @@ const SignInScreen = () => {
         <title>Sign In</title>
       </Helmet>
       <h1 className="my-3">Sign In</h1>
-      <Form>
+      <Form onSubmit={handleSignIn}>
         <Form.Group className="mb-3" controlId="email">
           <Form.Label>Email</Form.Label>
-          <Form.Control className="fs-3" type="email" required />
+          <Form.Control
+            className="fs-3"
+            type="email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="password">
           <Form.Label>Password</Form.Label>
-          <Form.Control className="fs-3" type="password" required />
+          <Form.Control
+            className="fs-3"
+            type="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form.Group>
         <div className="mb-3">
           <button style={{ paddingLeft: "20px", paddingRight: "20px" }} type="submit">
--- frontend/src/components/Store.js
+++ frontend/src/components/Store.js
@@ -24,6 +24,9 @@ const reducer = (state, action) => {
       localStorage.setItem("cartItems", JSON.stringify(cartItems));
       return { ...state, cart: { ...state.cart, cartItems } };
     }
+    case "USER_SIGNIN": {
+      return { ...state, userInfo: action.payload };
+    }
     default:
       return state;
   }
